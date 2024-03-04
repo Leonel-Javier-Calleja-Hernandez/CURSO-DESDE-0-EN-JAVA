51b3e85a7b72af3b6d695852c733131e980b6253
leonel javier calleja hernandez <leocallejas565@gmail.com>
2024-03-04 14:07:59 -0500

    GUARDADO/04/02/2024:02:00-if,if else,else

--- CURSO-CodeGym-3x3/juegoTresEnRaya.java
+++ CURSO-CodeGym-3x3/juegoTresEnRaya.java
@@ -1,10 +1,33 @@
 package es.codegym.games.ticktacktoe;
 
 import com.codegym.engine.cell.*;
-
+//  Tenemos un método que se encarga de mostrar una celda.
 public class TicTacToeGame extends Game {
+    
+    private int [][] model = new int [3][3];
+    private int currentPlayer;
+    
+    public void startGame(){
+        currentPlayer = 1;
+        
+        for (int x = 0; x<3 ; x++)
+            for(int y = 0; y<3; y++)
+                model[x][y]= 0;
+    }
     
     public void initialize(){
         setScreenSize(3,3);
+        startGame();
     }
-}
+    
+    public void updateCellView(int x, int y, int value){
+        if (value == 1)
+            setCellValue(x, y, "X");
+        else if (value == 2)
+            setCellValue(x, y, "O");
+        else 
+        setCellValue(x, y, " ");
+        /* Si el value es 0, muestre una línea vacía en la celda - " " (espacio),
+        Si el value es 1, muestre una cruz "X" en la celda. (gran X latina),
+        Si el value es 2, muestre el cero "O" en la celda. (o latina grande).*/
+    }
