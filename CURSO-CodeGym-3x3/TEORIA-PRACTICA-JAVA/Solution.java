3084baa47051a3a7fd824bacfcfa845443bdf58f
leonel javier calleja hernandez <leocallejas565@gmail.com>
2024-03-04 15:37:57 -0500

    GUARDADO/04/02/2024:03:33-TEORIA-PRACTICA-JAVA

---
+++ CURSO-CodeGym-3x3/TEORIA-PRACTICA-JAVA/Solution.java
@@ -0,0 +1,15 @@
+package es.codegym.task.task01.task0142;
+
+/* 
+Amigo es muy listo
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //escribe aquí tu código
+        System.out.print("Amigo es muy listo");
+    
+
+    }
+}
+// es una linea de comandos que muestra en consola("string(texto)")
