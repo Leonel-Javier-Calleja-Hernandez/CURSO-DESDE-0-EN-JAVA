8e4332bb761a20e136e40ea380f39bc9b391c358
leonel javier calleja hernandez <leocallejas565@gmail.com>
2024-03-04 12:03:08 -0500

    GUARDADO/04/02/2024:11:58-DibujandoLasCuadriculas3x3

---
+++ CURSO-CodeGym-3x3/juegoTresEnRaya.java
@@ -0,0 +1,10 @@
+package es.codegym.games.ticktacktoe;
+
+import com.codegym.engine.cell.*;
+
+public class TicTacToeGame extends Game {
+    
+    public void initialize(){
+        setScreenSize(3,3);
+    }
+}
