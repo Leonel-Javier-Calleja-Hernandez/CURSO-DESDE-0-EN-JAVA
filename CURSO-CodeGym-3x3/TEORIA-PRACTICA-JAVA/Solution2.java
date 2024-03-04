32399008636ad44c53c51be1ff9f2fcc35c628b8
leonel javier calleja hernandez <leocallejas565@gmail.com>
2024-03-04 16:03:34 -0500

    GUARDADO/04/02/2024:03:33-diferencia-entre-println-y-print

---
+++ CURSO-CodeGym-3x3/TEORIA-PRACTICA-JAVA/Solution2.java
@@ -0,0 +1,12 @@
+package es.codegym.task.task01.task0101;
+
+/* 
+Creo que ser programador es genial
+*/
+
+public class Solution2 {
+    public static void main(String[] args) {
+        //escribe aquí tu código
+        System.out.print("Creo que ser programador es genial");
+    }
+}
