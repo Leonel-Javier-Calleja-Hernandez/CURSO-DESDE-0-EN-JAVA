0e10f8cff42db5009e085b0175511bfc53a4372c
leonel javier calleja hernandez <leocallejas565@gmail.com>
2024-03-04 16:24:39 -0500

    GUARDADO/04/02/2024:03:33-Variables-Solution3

---
+++ CURSO-CodeGym-3x3/TEORIA-PRACTICA-JAVA/Solucion3.java
@@ -0,0 +1,14 @@
+package es.codegym.task.task01.task0109;
+
+/* 
+Declaramos solemnemente estas variables...
+*/
+
+public class Solution3 {
+    public static void main(String[] args) {
+        //escribe aquí tu código
+        int a = 1;
+        int b = 2;
+        
+    }
+}
