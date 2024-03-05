f5075aeb3b6c83efad941c9a04df5646f911b98a
leonel javier calleja hernandez <leocallejas565@gmail.com>
2024-03-05 13:07:32 -0500

    GUARDADO/04/02/2024:03:33-TicTacToEGame-9.1

--- CURSO-CodeGym-3x3/TicTacToeGame.java
+++ CURSO-CodeGym-3x3/TicTacToeGame.java
@@ -1,18 +1,24 @@
 package es.codegym.games.ticktacktoe;
 
-import com.codegym.engine.cell.*;
+import com.codegym.engine.cell.Color;
+import com.codegym.engine.cell.Game;
 
 public class TicTacToeGame extends Game {
     
     private int [][] model = new int [3][3];
     private int currentPlayer;
+    // se creeo una variable boolean para true y false
+    private boolean isGameStopped;
     
     public void startGame(){
         currentPlayer = 1;
+        // false_el juego continua
+        isGameStopped = false;
         
         for (int x = 0; x<3 ; x++)
             for(int y = 0; y<3; y++)
                 model[x][y]= 0;
+    
     }
     
     public void initialize(){
@@ -21,17 +27,17 @@ public class TicTacToeGame extends Game {
         updateView();
     }
     
-    public void updateCellView(int x, int y, int value){
+    public void updateCellView(int x, int y, int value) {
         if (value == 1)
-            setCellValue(x, y, "X");
+            setCellValueEx(x, y, Color.WHITE, "X", Color.RED);
         else if (value == 2)
-            setCellValue(x, y, "O");
-        else 
-        setCellValue(x, y, " ");
+            setCellValueEx(x, y, Color.WHITE, "O", Color.BLUE);
+        else
+            setCellValueEx(x, y, Color.WHITE, " ", Color.WHITE);
+    }
         /* Si el value es 0, muestre una línea vacía en la celda - " " (espacio),
         Si el value es 1, muestre una cruz "X" en la celda. (gran X latina),
         Si el value es 2, muestre el cero "O" en la celda. (o latina grande).*/
-    }
     
     // método que mostrará todas las celdas de la matriz model en la pantalla o tablero de juego
     public void updateView(){
@@ -43,7 +49,10 @@ public class TicTacToeGame extends Game {
     /*el método public void onMouseLeftClick(int x, int y). El motor del juego lo llamará cada 
     vez que el usuario haga clic en el campo de juego. También transmitirá las coordenadas de la celda 
     en la que el usuario hizo clic con el mouse; (x,y).*/
-   public void onMouseLeftClick(int x, int y){
+    public void onMouseLeftClick(int x, int y){
+        // true_el juego se detiene
+        if(isGameStopped)
+            return;
         if (model[x][y] != 0)
             return;
         model[x][y] = currentPlayer;
